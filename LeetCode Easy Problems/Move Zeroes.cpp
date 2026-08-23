08c8ee91248d2f13f85f9a52a69c1044991c16d5
Abhinav Bute <abhinavbute9@gmail.com>
2026-08-23 17:48:13 +0530

    Add files via upload

---
+++ LeetCode Easy Problems/Move Zeroes.cpp
@@ -0,0 +1,57 @@
+/*** Problem Statement 283:
+ Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+ 
+***/
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i]!=0){
+            nums[count] = nums[i];
+            count++;
+            }
+        }
+        for(int i = count; i < nums.size(); i++){
+            nums[i] = 0;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            cout << nums[i];
+        }
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> nums(6);
+    for(int i = 0; i < 6; i++){
+        cout << "Enter the element at the index " << i << " : ";
+        cin >> nums[i];
+    }
+    s.moveZeroes(nums);
+    return 0;
+}
